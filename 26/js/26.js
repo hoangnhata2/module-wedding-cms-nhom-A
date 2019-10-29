fcb41a843e1b7f569b0bf87364b4c4610c3b0444
CongTuan0807 <lctuan.0807@gmail.com>
2019-10-29 09:57:28 +0700

    fix

--- 26/js/26.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('.love div').click(function() {
-        var number = parseInt($(this).children('span').text());
-        $(this).children('.fa-heart').css({ 'color': '#f17677', 'font-size': '14px', 'transition': 'all .3s ease-in' });
-        number += 1;
-        $(this).children('span').replaceWith('<span>' + number + '</span>');
-    })
-})
